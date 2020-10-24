dfaeb06ee1a360b70c63da5f673ea0f8e5ea4ec4
scottfalbo <scottfalboart@gmail.com>
2020-10-24 10:42:57 -0700

    broke 500 error out into it's own function

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const pg = require('pg');
 const cors = require('cors');
 require('dotenv').config();
 const superagent = require('superagent');
+const { response } = require('express'); //eslint-disable-line
 // const { response } = require('express');
 
 const PORT = process.env.PORT || 3000;
@@ -26,17 +27,31 @@ function handleLocation(request, response){
   let city = request.query.city;
   let key = process.env.GEOCODE_API_KEY;
 
-  const URL = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
-
-  superagent.get(URL)
+  const checkSQL = `SELECT * FROM location`;
+  client.query(checkSQL)
     .then(data => {
-      let location =new Location(data.body[0], city);
-      response.status(200).send(location);
-    }).catch(error => {
-      response.status(500).send('Poop, something went wrong, ...');
-      console.log(error);
-    });
+      let dataCheck = data.rows.filter(value => value.search_query === city);
+      if (dataCheck[0]){
+        response.status(200).send(dataCheck[0]);
+      } else {
+        const URL = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
+        superagent.get(URL)
+          .then(data => {
+            let location = new Location(data.body[0], city);
+            response.status(200).send(location);
+
+            const SQL = 'INSERT INTO location (search_query, formatted_query, latitude, longitude) VALUES($1, $2, $3, $4) RETURNING *';
+            const safeValues = [location.search_query, location.formatted_query, location.latitude, location.longitude];
+            client.query(SQL, safeValues)
+              .then(data => { //eslint-disable-line
+                //store data
+              });
+          });
+      }
+    })
+    .catch( error => error500(request, response, error));
 }
+
 
 //--------------------- Weather handler
 function handleWeather(request, response){
@@ -55,10 +70,8 @@ function handleWeather(request, response){
         return new Weather(daily);
       });
       response.status(200).send(weatherArray);
-    }).catch(error => {
-      response.status(500).send('Poop, something went wrong, ...');
-      console.log(error);
-    });
+    })
+    .catch( error => error500(request, response, error));
 }
 
 // -------------------- Trails Handler
@@ -77,10 +90,8 @@ function handleTrails(request, response){
         return new Trails(newTrail);
       });
       response.status(200).send(trails);
-    }).catch(error => {
-      response.status(500).send('Poop, something went wrong, ...');
-      console.log(error);
-    });
+    })
+    .catch( error => error500(request, response, error));
 }
 
 // ----- Location constructor
@@ -111,8 +122,15 @@ function Trails(obj){
   this.condition_time = obj.condition_time;
 }
 
+//---------- Error messages---------------
+// ---------------------------------------- 500
+function error500(req, res, err) {
+  console.log('ERROR:', err);
+  res.status(500).send('function message');
+}
+//----------------------------------------- 404
 function notFound(request, response) {
-  response.status(404).send(`Couldn't load the thing into the thing from the other thing`);
+  response.status(404).send(`Couldn't load the thing into the thing from the other thing because there are no things here to be found.`);
 }
 
 // ------------------------- Connect to database
@@ -125,8 +143,3 @@ client.connect()
   .catch(error => {
     console.log('error message:', error);
   });
-
-// ----------------- Start Server
-// app.listen(PORT, () => {
-//   console.log(`It's Alive!!!`);
-// });
